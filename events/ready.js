dfcf70eb5f0154b1ba0c7f26e3b239722ba624ea
Lloyd <93144136+lloydnip@users.noreply.github.com>
2026-07-14 22:34:37 +0800

    Enhance client ready event with presence status
    Updated the client ready event to set user presence with a custom status message.

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,76 @@
-const { Events } = require("discord.js");
+const {
+    Events,
+    ActivityType
+} = require("discord.js");
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
 
-    execute(client) {
-        console.log(`${client.user.tag} is online!`);
+    async execute(client) {
+
+        console.log(`[INFO]: ${client.user.tag} is online!`);
+
+        client.user.setPresence({
+            status: "dnd", // online, idle, dnd, invisible
+            activities: [
+                {
+                    name: "Patch Updates: v0.0.2",
+                    type: ActivityType.Watching
+                }
+            ]
+        });
+
     }
 };
+
+/*
+const {
+    Events,
+    ActivityType
+} = require("discord.js");
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+
+    async execute(client) {
+
+        console.log(`[INFO]: ${client.user.tag} is online!`);
+
+        const statuses = [
+            {
+                name: "/help",
+                type: ActivityType.Watching
+            },
+            {
+                name: `${client.guilds.cache.size} Servers`,
+                type: ActivityType.Playing
+            },
+            {
+                name: `${client.users.cache.size} Users`,
+                type: ActivityType.Watching
+            },
+            {
+                name: "Axiora v0.0.1",
+                type: ActivityType.Listening
+            }
+        ];
+
+        let i = 0;
+
+        setInterval(() => {
+
+            client.user.setActivity(
+                statuses[i].name,
+                { type: statuses[i].type }
+            );
+
+            i++;
+            if (i >= statuses.length) i = 0;
+
+        }, 10000);
+
+    }
+};
+*/
