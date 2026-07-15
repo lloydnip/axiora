60e507d6818e4fca1e1af6e8f5d8b80f85638999
Lloyd <93144136+lloydnip@users.noreply.github.com>
2026-07-15 16:34:13 +0800

    Add verification configuration functions

---
+++ utils/verification.js
@@ -0,0 +1,42 @@
+const { loadJSON, saveJSON } = require("./database");
+
+function getConfig(guildId) {
+
+    const db = loadJSON("verification.json");
+
+    if (!db[guildId]) {
+
+        db[guildId] = {
+
+            enabled: false,
+
+            channel: null,
+
+            role: null,
+
+            logChannel: null
+
+        };
+
+        saveJSON("verification.json", db);
+
+    }
+
+    return db[guildId];
+
+}
+
+function saveConfig(guildId, config) {
+
+    const db = loadJSON("verification.json");
+
+    db[guildId] = config;
+
+    saveJSON("verification.json", db);
+
+}
+
+module.exports = {
+    getConfig,
+    saveConfig
+};
