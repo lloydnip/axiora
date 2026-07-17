ada91f7e892ffb8bbf72072d451bd2f5083e84fa
Lloyd <93144136+lloydnip@users.noreply.github.com>
2026-07-17 14:35:44 +0800

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,23 @@
 const { Events } = require("discord.js");
-const { sendWelcome } = require("../utils/welcome");
+const embedManager = require("../utils/embedManager");
+const buildWelcomeEmbed = require("../utils/welcomeEmbed");
 
 module.exports = {
     name: Events.GuildMemberAdd,
 
     async execute(member) {
-        await sendWelcome(member);
+
+        const config = embedManager.load("welcome");
+
+        if (!config.channel) return;
+
+        const channel = member.guild.channels.cache.get(config.channel);
+
+        if (!channel) return;
+
+        const message = buildWelcomeEmbed(member);
+
+        await channel.send(message);
+
     }
 };
