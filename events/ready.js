ebed2e09917aca0fc10fd99fdd0c484db3b37ca3
Lloyd <93144136+lloydnip@users.noreply.github.com>
2026-07-17 14:39:00 +0800

    Refactor client ready event with new statuses
    Updated bot presence and activity statuses.

--- events/ready.js
+++ events/ready.js
@@ -11,38 +11,20 @@ module.exports = {
 
         console.log(`[INFO]: ${client.user.tag} is online!`);
 
+
         client.user.setPresence({
-            status: "dnd", // online, idle, dnd, invisible
+            status: "dnd",
             activities: [
                 {
-                    name: "Patch Updates: v0.0.2",
-                    type: ActivityType.Watching
+                    name: "Axiora v0.0.3",
+                    type: ActivityType.Listening
                 }
             ]
         });
 
-    }
-};
 
-/*
-const {
-    Events,
-    ActivityType
-} = require("discord.js");
-
-module.exports = {
-    name: Events.ClientReady,
-    once: true,
-
-    async execute(client) {
-
-        console.log(`[INFO]: ${client.user.tag} is online!`);
 
         const statuses = [
-            {
-                name: "/help",
-                type: ActivityType.Watching
-            },
             {
                 name: `${client.guilds.cache.size} Servers`,
                 type: ActivityType.Playing
@@ -50,27 +32,33 @@ module.exports = {
             {
                 name: `${client.users.cache.size} Users`,
                 type: ActivityType.Watching
-            },
-            {
-                name: "Axiora v0.0.1",
-                type: ActivityType.Listening
             }
         ];
 
+
         let i = 0;
+
 
         setInterval(() => {
 
             client.user.setActivity(
                 statuses[i].name,
-                { type: statuses[i].type }
+                {
+                    type: statuses[i].type
+                }
             );
+            
+            client.user.setStatus("dnd");
+
 
             i++;
-            if (i >= statuses.length) i = 0;
+
+            if (i >= statuses.length) {
+                i = 0;
+            }
+
 
         }, 10000);
 
     }
 };
-*/
